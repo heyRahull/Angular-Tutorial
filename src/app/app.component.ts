88020f695c3d84b857ceab36e29fe04e62a76567
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-05-10 13:03:36 +0530

    Data Binding in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SuccessComponent } from './success/success.component';
 import { WarningComponent } from './warning/warning.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SuccessComponent, WarningComponent],
+  imports: [RouterOutlet, SuccessComponent, WarningComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,4 +15,6 @@ export class AppComponent {
   title = 'my-first-project';
   name = 'The Humble Coder';
   count = 3;
+  username = 'rahul';
+  isdisabled = true;
 }
